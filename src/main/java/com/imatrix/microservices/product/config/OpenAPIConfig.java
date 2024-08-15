735db5686bef2f0f3cb56652cf2f86c5e97a3f10
mnpsnuwan <nuwanp124@gmail.com>
2024-08-14 20:18:59 -0700

    IM:DEV:Product Service - Reformatted Code.

--- src/main/java/com/imatrix/microservices/product/config/OpenAPIConfig.java
+++ src/main/java/com/imatrix/microservices/product/config/OpenAPIConfig.java
@@ -23,11 +23,11 @@ public class OpenAPIConfig
     {
         return new OpenAPI()
                        .info( new Info().title( "Product Service API" )
-                                      .description( "This is the REST API for Product Service" )
-                                      .version( "v0.0.1" )
-                                      .license( new License().name( "Apache 2.0" ) ))
+                                        .description( "This is the REST API for Product Service" )
+                                        .version( "v0.0.1" )
+                                        .license( new License().name( "Apache 2.0" ) ) )
                        .externalDocs( new ExternalDocumentation()
                                               .description( "You can refer to the Product Service Wiki Documentation" )
-                                              .url( "https://product-service-dummy-url.com/docs" ));
+                                              .url( "https://product-service-dummy-url.com/docs" ) );
     }
 }
